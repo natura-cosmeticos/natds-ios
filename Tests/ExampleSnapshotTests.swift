72316e2f12ddc6d365c50ab6d8d993af97f411d5
Vivian Soares <vcsoares@thoughtworks.com>
2019-11-21 18:58:41 -0300

    Add git lfs to pipeline

--- Tests/ExampleSnapshotTests.swift
+++ Tests/ExampleSnapshotTests.swift
@@ -4,6 +4,7 @@ class ExampleSnapshotTests: FBSnapshotTestCase {
     func testExample() {
         let sut = UIView(frame: CGRect(x: 0, y: 0, width: 64, height: 64))
         sut.backgroundColor = UIColor.blue
+
         FBSnapshotVerifyView(sut)
     }
 }
