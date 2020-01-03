eae1319c9910eab99870c500cf5d40be2adb5eac
Jhonathan Wyterlin <jwyterlin@thoughtworks.com>
2020-01-03 11:46:00 -0300

    Create new font H5

--- Sources/DesignTokens/Fonts/Branding/FontsNatura.swift
+++ Sources/DesignTokens/Fonts/Branding/FontsNatura.swift
@@ -1,4 +1,5 @@
 struct FontsNatura {
+    static let h5: UIFont = UIFont.systemFont(ofSize: 24, weight: .regular)
     static let h6: UIFont = UIFont.systemFont(ofSize: 20, weight: .medium)
     static let body2: UIFont = UIFont.systemFont(ofSize: 14, weight: .regular)
     static let button: UIFont = UIFont.systemFont(ofSize: 14, weight: .medium)
--- Sources/DesignTokens/Fonts/Fonts.swift
+++ Sources/DesignTokens/Fonts/Fonts.swift
@@ -1,5 +1,6 @@
 @objcMembers
 public class Fonts: NSObject {
+    public static let h5: UIFont = FontsNatura.h5
     public static let h6: UIFont = FontsNatura.h6
     public static let body2: UIFont = FontsNatura.body2
     public static let button: UIFont = FontsNatura.button
