3753acda3530dade744a85f9e56c30f9bb6d9264
Érico Perez Neto <ericopneto@offtopic.io>
2020-02-13 11:19:55 -0300

    Fix Divider swiftlint divider

--- Sources/Components/Divider/Divider.swift
+++ Sources/Components/Divider/Divider.swift
@@ -4,7 +4,7 @@ public class Divider: UIView {
         super.init(frame: .zero)
         setup()
     }
-    
+
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
