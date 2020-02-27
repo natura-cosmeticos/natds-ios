37e1bda5c824d09459ed49bc3d055684f63bc300
Vitor Mesquita <vitor.mesquita09@gmail.com>
2020-02-27 12:15:51 -0300

    creating unit tests to UITextifieldDelegate on TextField

---
+++ Tests/Components/Field/Mock/TextFieldDelegateMock.swift
@@ -0,0 +1,32 @@
+import Foundation
+@testable import NatDS
+
+class TextFieldDelegateMock: TextFieldDelegate {
+
+    var invokedDidBeginEditing: (count: Int, field: TextField?) = (count: 0, field: nil)
+    var invokedDidEndEditing: (count: Int, field: TextField?) = (count: 0, field: nil)
+    var invokedDidShouldBeginEditing: (count: Int, field: TextField?) = (count: 0, field: nil)
+    var invokedTextFieldChangeCharInRange: (count: Int, field: TextField?) = (count: 0, field: nil)
+
+    func natTextFieldDidBeginEditing(_ textField: TextField) {
+        invokedDidBeginEditing.count += 1
+        invokedDidBeginEditing.field = textField
+    }
+
+    func natTextFieldDidEndEditing(_ textField: TextField) {
+        invokedDidEndEditing.count += 1
+        invokedDidEndEditing.field = textField
+    }
+
+    func natTextFieldShouldBeginEditing(_ textField: TextField) -> Bool {
+        invokedDidShouldBeginEditing.count += 1
+        invokedDidShouldBeginEditing.field = textField
+        return true
+    }
+
+    func natTextField(_ textField: TextField, changeCharInRange: NSRange, string: String) -> Bool {
+        invokedTextFieldChangeCharInRange.count += 1
+        invokedTextFieldChangeCharInRange.field = textField
+        return true
+    }
+}
