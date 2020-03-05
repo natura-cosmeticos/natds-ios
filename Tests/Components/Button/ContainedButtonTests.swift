2fc415583960bfebc64b7422e0f07e26d5486a8c
Vitor Mesquita <vitor.mesquita09@gmail.com>
2020-03-05 17:14:55 -0400

    returing settile test on ContainedButton and fixing tabitemView constraints

--- Tests/Components/Button/ContainedButtonTests.swift
+++ Tests/Components/Button/ContainedButtonTests.swift
@@ -26,4 +26,9 @@ class ContainedButtonTests: FBSnapshotTestCase {
         sut.isEnabled = false
         FBSnapshotVerifyView(superview)
     }
+
+    func test_setTitle_hasUppercasedTitle() {
+        sut.setTitle("ok", for: .normal)
+        XCTAssertEqual(sut.titleLabel?.text, "OK")
+    }
 }
