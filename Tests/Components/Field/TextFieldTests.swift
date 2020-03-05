94ce79b8d19871cf92ac7c58a78780975ea2c6cd
Érico Perez Neto <ericopneto@offtopic.io>
2020-03-05 15:43:47 -0300

    Remove recordMode

--- Tests/Components/Field/TextFieldTests.swift
+++ Tests/Components/Field/TextFieldTests.swift
@@ -35,7 +35,6 @@ class TextFieldTests: FBSnapshotTestCase {
     }
 
     func test_state_whenHasError_returnMultipleErrorLinesStateSnapshot() {
-        recordMode = true
         sut.frame = CGRect(x: 0, y: 0, width: 328, height: 119)
         sut.error = "Error: Has an error - this input has an error - Neque porro quisquam est qui dolorem ipsum quia"
         FBSnapshotVerifyView(sut)
