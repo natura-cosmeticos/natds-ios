f434ab03de12604adb04668f49f4e2f87399c6b1
rov <valadares.thel@gmail.com>
2020-04-21 16:21:51 -0300

    Creates tests for DesignSystem class

---
+++ Tests/Public/DesignSystem+Spec.swift
@@ -0,0 +1,36 @@
+import Quick
+import Nimble
+
+@testable import NatDS
+
+final class DesignSystemSpec: QuickSpec {
+    override func spec() {
+        describe("DesignSystem") {
+            let designSystem = DesignSystem.shared
+
+            describe("method .configure") {
+                context("Brand: .avon") {
+                    beforeEach {
+                        designSystem.configure(brand: .avon)
+                    }
+
+                    it("sets current theme to Avon") {
+                        let theme = designSystem.currentTheme
+                        expect(theme).to(beAnInstanceOf(AvonTheme.self))
+                    }
+                }
+
+                context("Brand: .theBodyShop") {
+                    beforeEach {
+                        designSystem.configure(brand: .theBodyShop)
+                    }
+
+                    it("sets current theme to The Body Shop") {
+                        let theme = designSystem.currentTheme
+                        expect(theme).to(beAnInstanceOf(TheBodyShopTheme.self))
+                    }
+                }
+            }
+        }
+    }
+}
