72ec3a7db16283fd9de79aa70b7a191b126cd8a1
rov <valadares.thel@gmail.com>
2020-04-23 16:24:07 -0300

    Adds tests for TBS, Avon and Natura Surface color palette

---
+++ Tests/Core/SupportedBrands/Avon/ColorPaletteDark/AvonColorPaletteDark+Surface+Spec.swift
@@ -0,0 +1,46 @@
+import Quick
+import Nimble
+
+@testable import NatDS
+
+final class AvonColorPaletteDarkSurfaceSpec: QuickSpec {
+    override func spec() {
+        let systemUnderTest = AvonColorPaletteDark.Surface()
+
+        describe("#background") {
+            it("returns a string of a color hex expected for background color") {
+                let colorHex = systemUnderTest.background
+                let expectedHex = "#121212"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#onBackground") {
+            it("returns a string of a color hex expected for onBackground color") {
+                let colorHex = systemUnderTest.onBackground
+                let expectedHex = "#FFFFFF"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#surface") {
+            it("returns a string of a color hex expected for surface color") {
+                let colorHex = systemUnderTest.surface
+                let expectedHex = "#333333"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#onSurface") {
+            it("returns a string of a color hex expected for onSurface color") {
+                let colorHex = systemUnderTest.onSurface
+                let expectedHex = "#FFFFFF"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+    }
+}
---
+++ Tests/Core/SupportedBrands/Avon/ColorPaletteLight/AvonColorPaletteLight+Surface+Spec.swift
@@ -0,0 +1,46 @@
+import Quick
+import Nimble
+
+@testable import NatDS
+
+final class AvonColorPaletteLightSurfaceSpec: QuickSpec {
+    override func spec() {
+        let systemUnderTest = AvonColorPaletteLight.Surface()
+
+        describe("#background") {
+            it("returns a string of a color hex expected for background color") {
+                let colorHex = systemUnderTest.background
+                let expectedHex = "#FAFAFA"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#onBackground") {
+            it("returns a string of a color hex expected for onBackground color") {
+                let colorHex = systemUnderTest.onBackground
+                let expectedHex = "#333333"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#surface") {
+            it("returns a string of a color hex expected for surface color") {
+                let colorHex = systemUnderTest.surface
+                let expectedHex = "#FFFFFF"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#onSurface") {
+            it("returns a string of a color hex expected for onSurface color") {
+                let colorHex = systemUnderTest.onSurface
+                let expectedHex = "#333333"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+    }
+}
