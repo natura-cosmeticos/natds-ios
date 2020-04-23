ce6d6c47556bc55dc3c01dbf7e3ba5b0f74425b8
rov <valadares.thel@gmail.com>
2020-04-23 16:40:33 -0300

    Adds tests for TBS, Avon and Natura Content color palette

---
+++ Tests/Core/SupportedBrands/Avon/ColorPaletteDark/AvonColorPaletteDark+Content+Spec.swift
@@ -0,0 +1,64 @@
+import Quick
+import Nimble
+
+@testable import NatDS
+
+final class AvonColorPaletteDarkContentSpec: QuickSpec {
+    override func spec() {
+        let systemUnderTest = AvonColorPaletteDark.Content()
+
+        describe("#highlight") {
+            it("returns a string of a color hex expected for highlight color") {
+                let colorHex = systemUnderTest.highlight
+                let expectedHex = "#FFFFFF"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#highEmphasis") {
+            it("returns a string of a color hex expected for highEmphasis color") {
+                let colorHex = systemUnderTest.highEmphasis
+                let expectedHex = "#FAFAFA"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#mediumEmphasis") {
+            it("returns a string of a color hex expected for mediumEmphasis color") {
+                let colorHex = systemUnderTest.mediumEmphasis
+                let expectedHex = "#BBBBBB"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#lowEmphasis") {
+            it("returns a string of a color hex expected for lowEmphasis color") {
+                let colorHex = systemUnderTest.lowEmphasis
+                let expectedHex = "#777777"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#link") {
+            it("returns a string of a color hex expected for link color") {
+                let colorHex = systemUnderTest.link
+                let expectedHex = "#227BBD"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+
+        describe("#onLink") {
+            it("returns a string of a color hex expected for onLink color") {
+                let colorHex = systemUnderTest.onLink
+                let expectedHex = "#FFFFFF"
+
+                expect(colorHex == expectedHex).to(beTrue())
+            }
+        }
+    }
+}
