eef5b82ac7e411f7f3799930a8a11c34ba1d98c1
rov <valadares.thel@gmail.com>
2020-04-30 21:57:21 -0300

    Adding public to DSSpacing properties and adding documentation

--- Sources/Public/DesignTokens/DSSpacing.swift
+++ Sources/Public/DesignTokens/DSSpacing.swift
@@ -1,13 +1,31 @@
-import Foundation
+/**
+  DSSpacing is a struct that has access to spacing properties from the Design System.
+  This properties are used with contraints to handle view spaces between other views or view properties like
+  safeAreaLayoutGuide.
+  According with the current Brand in the Design System this spacing properties can change.
+
+    All properties returns a CGFloat.
+
+    Exemple of usage:
+
+        containerView.topAnchor.constraint(equalTo: view.topAnchor, constant: DSSpacing.tiny)
+        containerView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: DSSpacing.large)
+
+ - Requires:
+        It's necessary to configure the Design System current Brand at DesisgnSystem class
+        or fatalError will be raised.
+
+            DesignSystem().configure(with: Brand)
+*/
 
 public struct DSSpacing {
     private init() {}
 
-    static var micro: CGFloat { getCurrentTheme().spacing.micro }
-    static var tiny: CGFloat { getCurrentTheme().spacing.tiny }
-    static var small: CGFloat { getCurrentTheme().spacing.small }
-    static var standart: CGFloat { getCurrentTheme().spacing.standart }
-    static var semi: CGFloat { getCurrentTheme().spacing.semi }
-    static var large: CGFloat { getCurrentTheme().spacing.large }
-    static var xLarge: CGFloat { getCurrentTheme().spacing.xLarge }
+    public static var micro: CGFloat { getCurrentTheme().spacing.micro }
+    public static var tiny: CGFloat { getCurrentTheme().spacing.tiny }
+    public static var small: CGFloat { getCurrentTheme().spacing.small }
+    public static var standart: CGFloat { getCurrentTheme().spacing.standart }
+    public static var semi: CGFloat { getCurrentTheme().spacing.semi }
+    public static var large: CGFloat { getCurrentTheme().spacing.large }
+    public static var xLarge: CGFloat { getCurrentTheme().spacing.xLarge }
 }
