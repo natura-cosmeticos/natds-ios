41a243fa35f386aea22f33fea5957010af7d6200
rov <valadares.thel@gmail.com>
2020-05-05 20:24:39 -0300

    Adds sample app to DSSize

---
+++ SampleApp/Sources/Sample/Design/Size/Cells/SizeCell.swift
@@ -0,0 +1,79 @@
+import UIKit
+import NatDS
+
+final class SizeCell: UITableViewCell {
+
+    // MARK: - Private properties
+
+    private let symbolicSizeView: UIView = {
+        let view = UIView()
+        view.backgroundColor = DSColors.primary
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private let label: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+
+        return label
+    }()
+
+    private var symbolicSizeWidthConstraint: NSLayoutConstraint!
+
+    // MARK: - Inits
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+
+        setup()
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - Public methods
+
+    func configure(description: String, size: CGFloat) {
+        label.text = description
+
+        symbolicSizeWidthConstraint.constant = size
+        symbolicSizeView.layoutIfNeeded()
+    }
+
+    // MARK: - Private methods
+
+    private func setup() {
+        contentView.addSubview(symbolicSizeView)
+        contentView.addSubview(label)
+
+        symbolicSizeWidthConstraint = symbolicSizeView
+            .widthAnchor.constraint(equalToConstant: 0)
+
+        NSLayoutConstraint.activate([
+            label.topAnchor.constraint(
+                equalTo: contentView.topAnchor,
+                constant: 4
+            ),
+            label.leftAnchor.constraint(
+                equalTo: contentView.leftAnchor
+            ),
+
+            symbolicSizeView.topAnchor.constraint(
+                equalTo: label.bottomAnchor,
+                constant: 4
+            ),
+            symbolicSizeView.bottomAnchor.constraint(
+                equalTo: contentView.bottomAnchor,
+                constant: -4
+            ),
+            symbolicSizeView.leftAnchor.constraint(
+                equalTo: contentView.leftAnchor
+            ),
+            symbolicSizeWidthConstraint
+        ])
+    }
+}
---
+++ SampleApp/Sources/Sample/Design/Size/SizeViewController.swift
@@ -0,0 +1,94 @@
+import UIKit
+import NatDS
+
+final class SizeViewController: UIViewController, SampleItem {
+    static var name = "Size"
+
+    // MARK: - Private properties
+
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(SizeCell.self, forCellReuseIdentifier: SizeCell.reuseIdentifier)
+        tableView.rowHeight = 60
+        tableView.allowsSelection = false
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+
+        return tableView
+    }()
+
+    private let cellsViewModels: [(description: String, value: CGFloat)] = [
+        ("micro: \(DSSize.micro)", DSSize.micro),
+        ("tiny: \(DSSize.tiny)", DSSize.tiny),
+        ("small: \(DSSize.small)", DSSize.small),
+        ("standard: \(DSSize.standart)", DSSize.standart),
+        ("semi: \(DSSize.semi)", DSSize.semi),
+        ("semiX: \(DSSize.semiX)", DSSize.semiX),
+        ("medium: \(DSSize.medium)", DSSize.medium),
+        ("mediumX: \(DSSize.mediumX)", DSSize.mediumX),
+        ("large: \(DSSize.large)", DSSize.large),
+        ("largeX: \(DSSize.largeX)", DSSize.largeX),
+        ("largeXX: \(DSSize.largeXX)", DSSize.largeXX),
+        ("largeXXX: \(DSSize.largeXXX)", DSSize.largeXXX),
+        ("huge: \(DSSize.huge)", DSSize.huge),
+        ("hugeX: \(DSSize.hugeX)", DSSize.hugeX),
+        ("hugeXX: \(DSSize.hugeXX)", DSSize.hugeXX),
+        ("hugeXXX: \(DSSize.hugeXXX)", DSSize.hugeXXX),
+        ("veryHuge: \(DSSize.veryHuge)", DSSize.veryHuge)
+    ]
+
+    // MARK: - Life cycle
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        title = Self.name
+
+        setup()
+    }
+
+    // MARK: - Private methods
+
+    private func setup() {
+        view.backgroundColor = DSColors.background
+        view.addSubview(tableView)
+
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor
+            ),
+            tableView.rightAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.rightAnchor,
+                constant: -DSSpacing.tiny
+            ),
+            tableView.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor
+            ),
+            tableView.leftAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leftAnchor,
+                constant: DSSpacing.tiny
+            )
+        ])
+    }
+}
+
+// MARK: - UITableViewDataSource
+
+extension SizeViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        cellsViewModels.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(
+            withIdentifier: SizeCell.reuseIdentifier,
+            for: indexPath
+        ) as? SizeCell ?? SizeCell()
+
+        let viewModel = cellsViewModels[indexPath.row]
+
+        cell.configure(description: viewModel.description, size: viewModel.value)
+
+        return cell
+    }
+}
