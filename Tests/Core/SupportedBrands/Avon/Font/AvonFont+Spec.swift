23a64029f4639e76212414c94cf6e0fe386830b8
rov <valadares.thel@gmail.com>
2020-05-11 15:17:02 -0300

    Adds tests to theme's font for Brands

---
+++ Tests/Core/SupportedBrands/Avon/Font/AvonFont+Spec.swift
@@ -0,0 +1,26 @@
+import Quick
+import Nimble
+
+@testable import NatDS
+
+final class AvonFontSpec: QuickSpec {
+    override func spec() {
+        let systemUnderTest = AvonFont()
+
+        beforeEach {
+            DesignSystem().configure(with: .natura)
+        }
+
+        describe("#sizes") {
+            it("returns a instance of AvonFontSizes") {
+                expect(systemUnderTest.sizes).to(beAnInstanceOf(AvonFontSizes.self))
+            }
+        }
+
+        describe("#weights") {
+            it("returns a instance of AvonFontWeights") {
+                expect(systemUnderTest.weights).to(beAnInstanceOf(AvonFontWeights.self))
+            }
+        }
+    }
+}
