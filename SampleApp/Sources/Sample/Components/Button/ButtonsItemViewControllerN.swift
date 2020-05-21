71883cdee5787c33a57c41d5b7d3b1a017a8e4ed
rov <valadares.thel@gmail.com>
2020-05-21 13:57:38 -0300

    Creates estructure for sample app to show button

--- SampleApp/Sources/Sample/Components/Button/ButtonsItemViewControllerN.swift
+++ SampleApp/Sources/Sample/Components/Button/ButtonsItemViewControllerN.swift
@@ -1,11 +1,70 @@
 import NatDS
 
-class ButtonsItemViewControllerN: UIViewController, SampleItem {
+// swiftlint:disable function_body_length
+
+final class ButtonsItemViewControllerN: UIViewController, SampleItem {
     static var name = "Button N"
+
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+
+        return scrollView
+    }()
+
+    private let containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+
+        return view
+    }()
+
+    private lazy var eneabledStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.distribution = .fillEqually
+        stack.translatesAutoresizingMaskIntoConstraints = false
+
+        return stack
+    }()
+
+    private lazy var disabledStackView: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 16
+        stack.distribution = .fillEqually
+        stack.translatesAutoresizingMaskIntoConstraints = false
+
+        return stack
+    }()
+
+    private let containedButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Contained")
+
+        return button
+    }()
 
     private let outlinedButton: NatButton = {
         let button = NatButton(style: .outlined)
         button.configure(title: "Outlined")
+
+        return button
+    }()
+
+    private let textButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Text")
+
+        return button
+    }()
+
+    private let disableContainedButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Contained")
+        button.isEnabled = false
+
         return button
     }()
 
@@ -13,12 +72,37 @@ class ButtonsItemViewControllerN: UIViewController, SampleItem {
         let button = NatButton(style: .outlined)
         button.configure(title: "Outlined")
         button.isEnabled = false
+
+        return button
+    }()
+
+    private let disableTextButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Text")
+        button.isEnabled = false
+
+        return button
+    }()
+
+    private let containedFullButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Contained")
+
+        return button
+    }()
+
+    private let disableContainedFullButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Disabled Contained")
+        button.isEnabled = false
+
         return button
     }()
 
     private let outlinedFullButton: NatButton = {
         let button = NatButton(style: .outlined)
         button.configure(title: "Outlined")
+
         return button
     }()
 
@@ -26,6 +110,22 @@ class ButtonsItemViewControllerN: UIViewController, SampleItem {
         let button = NatButton(style: .outlined)
         button.configure(title: "Disabled Outlined")
         button.isEnabled = false
+
+        return button
+    }()
+
+    private let textFullButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Text")
+
+        return button
+    }()
+
+    private let disableTextFullButton: NatButton = {
+        let button = NatButton(style: .outlined)
+        button.configure(title: "Disabled Text")
+        button.isEnabled = false
+
         return button
     }()
 
@@ -38,43 +138,126 @@ class ButtonsItemViewControllerN: UIViewController, SampleItem {
 //            self.outlinedButton.configure(height: .medium)
 //        }
 
-        DispatchQueue.main.asyncAfter(deadline: .now() + 6.5) {
-            self.outlinedButton.isEnabled = false
-        }
-
-        DispatchQueue.main.asyncAfter(deadline: .now() + 12.5) {
-            self.outlinedButton.isEnabled = true
-        }
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 6.5) {
+//            self.outlinedButton.isEnabled = false
+//        }
+//
+//        DispatchQueue.main.asyncAfter(deadline: .now() + 12.5) {
+//            self.outlinedButton.isEnabled = true
+//        }
     }
 
     private func setup() {
         view.backgroundColor = NatColors.background
 
-        view.addSubview(outlinedButton)
-        view.addSubview(disableOutlinedButton)
-        view.addSubview(outlinedFullButton)
-        view.addSubview(disableOutlinedFullButton)
+        scrollView.addSubview(containerView)
+
+        eneabledStackView.addArrangedSubview(outlinedButton)
+        eneabledStackView.addArrangedSubview(containedButton)
+        eneabledStackView.addArrangedSubview(textButton)
+
+        disabledStackView.addArrangedSubview(disableContainedButton)
+        disabledStackView.addArrangedSubview(disableOutlinedButton)
+        disabledStackView.addArrangedSubview(disableTextButton)
+
+        containerView.addSubview(containedFullButton)
+        containerView.addSubview(disableContainedFullButton)
+
+        containerView.addSubview(outlinedFullButton)
+        containerView.addSubview(disableOutlinedFullButton)
+        containerView.addSubview(textFullButton)
+        containerView.addSubview(disableTextFullButton)
+
+        view.addSubview(scrollView)
+        view.addSubview(eneabledStackView)
+        view.addSubview(disabledStackView)
+
         addConstraints()
     }
 
     private func addConstraints() {
-//        stackView.translatesAutoresizingMaskIntoConstraints = false
+        let containerHeightConstraint = containerView.centerYAnchor
+            .constraint(equalTo: scrollView.centerYAnchor)
+        containerHeightConstraint.priority = .defaultLow
 
         let constraints = [
-            outlinedButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 16),
-            outlinedButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-//            outlinedButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
 
-            disableOutlinedButton.topAnchor.constraint(equalTo: outlinedButton.bottomAnchor, constant: 16),
-            disableOutlinedButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            containerView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            containerView.trailingAnchor.constraint(
+                equalTo: scrollView.trailingAnchor,
+                constant: -NatSpacing.small
+            ),
+            containerView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            containerView.leadingAnchor.constraint(
+                equalTo: scrollView.leadingAnchor,
+                constant: NatSpacing.small
+            ),
+            containerView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+            containerHeightConstraint,
 
-            outlinedFullButton.topAnchor.constraint(equalTo: disableOutlinedButton.bottomAnchor, constant: 16),
-            outlinedFullButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            outlinedFullButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            eneabledStackView.topAnchor.constraint(
+                equalTo: containerView.topAnchor,
+                constant: NatSpacing.small
+            ),
+            eneabledStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            eneabledStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
 
-            disableOutlinedFullButton.topAnchor.constraint(equalTo: outlinedFullButton.bottomAnchor, constant: 16),
-            disableOutlinedFullButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            disableOutlinedFullButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            disabledStackView.topAnchor.constraint(
+                equalTo: eneabledStackView.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            disabledStackView.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            disabledStackView.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+
+            containedFullButton.topAnchor.constraint(
+                equalTo: disabledStackView.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            containedFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            containedFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+
+            disableContainedFullButton.topAnchor.constraint(
+                equalTo: containedFullButton.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            disableContainedFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            disableContainedFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+
+            outlinedFullButton.topAnchor.constraint(
+                equalTo: disableContainedFullButton.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            outlinedFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            outlinedFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+
+            disableOutlinedFullButton.topAnchor.constraint(
+                equalTo: outlinedFullButton.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            disableOutlinedFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            disableOutlinedFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+
+            textFullButton.topAnchor.constraint(
+                equalTo: disableOutlinedFullButton.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            textFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            textFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+
+            disableTextFullButton.topAnchor.constraint(
+                equalTo: textFullButton.bottomAnchor,
+                constant: NatSpacing.small
+            ),
+            disableTextFullButton.trailingAnchor.constraint(equalTo: containerView.trailingAnchor),
+            disableTextFullButton.leadingAnchor.constraint(equalTo: containerView.leadingAnchor),
+            disableTextFullButton.bottomAnchor.constraint(
+                equalTo: containerView.bottomAnchor,
+                constant: -NatSpacing.small
+            )
         ]
 
         NSLayoutConstraint.activate(constraints)
