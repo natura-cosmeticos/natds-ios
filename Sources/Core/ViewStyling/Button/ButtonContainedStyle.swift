6a41dd2ed4de6ce1f177bb620ef4e8331f0c5179
rov <valadares.thel@gmail.com>
2020-06-02 16:38:54 -0300

    Creates ButtonContainedStyle

---
+++ Sources/Core/ViewStyling/Button/ButtonContainedStyle.swift
@@ -0,0 +1,13 @@
+enum ButtonContainedStyle {
+    static func applyStyle(on button: UIButton) {
+
+    }
+
+    static func applyStyleForStates(on button: UIButton) {
+
+    }
+
+    static func applyStyleForTitle(_ title: String?, on button: UIButton) {
+
+    }
+}
