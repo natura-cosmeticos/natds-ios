16680e28f69641eec91ab40549e9849beb2caccb
rov <valadares.thel@gmail.com>
2020-06-04 14:10:37 -0300

    Fixes typo: a expect -> an expected

--- Tests/Core/SupportedBrands/Natura/NaturaElevations+Spec.swift
+++ Tests/Core/SupportedBrands/Natura/NaturaElevations+Spec.swift
@@ -10,19 +10,19 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#none") {
             let attributes = systemUnderTest.none
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 expect(attributes.shadowColor).to(beNil())
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: -3)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(3))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0))
             }
         }
@@ -30,20 +30,20 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation01") {
             let attributes = systemUnderTest.elevation01
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 1)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(1))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -51,20 +51,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation02") {
             let attributes = systemUnderTest.elevation02
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 2)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(2))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -72,20 +73,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation03") {
             let attributes = systemUnderTest.elevation03
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 3)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(4))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -93,20 +95,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation04") {
             let attributes = systemUnderTest.elevation04
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 4)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(5))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -114,20 +117,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation05") {
             let attributes = systemUnderTest.elevation05
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 6)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(10))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -135,20 +139,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation06") {
             let attributes = systemUnderTest.elevation06
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 8)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(10))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -156,20 +161,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation07") {
             let attributes = systemUnderTest.elevation07
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 9)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(12))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -177,20 +183,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation08") {
             let attributes = systemUnderTest.elevation08
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 12)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(17))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -198,20 +205,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation09") {
             let attributes = systemUnderTest.elevation09
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 16)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(24))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
@@ -219,20 +227,21 @@ final class NaturaElevationsSpec: QuickSpec {
         describe("#elevation10") {
             let attributes = systemUnderTest.elevation10
 
-            it("returns a expect shadowColor") {
+            it("returns an expected shadowColor") {
                 let cgColor = UIColor(red: 0, green: 0, blue: 0, alpha: 1).cgColor
+
                 expect(attributes.shadowColor).to(equal(cgColor))
             }
 
-            it("returns a expect shadowOffSet") {
+            it("returns an expected shadowOffSet") {
                 expect(attributes.shadowOffSet).to(equal(.init(width: 0, height: 24)))
             }
 
-            it("returns a expect shadowRadius") {
+            it("returns an expected shadowRadius") {
                 expect(attributes.shadowRadius).to(equal(38))
             }
 
-            it("returns a expect shadowOpacity") {
+            it("returns an expected shadowOpacity") {
                 expect(attributes.shadowOpacity).to(equal(0.14))
             }
         }
