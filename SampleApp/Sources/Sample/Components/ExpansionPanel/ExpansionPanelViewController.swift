3d31006778855282c82ecb526f87c781f172d77f
Jhonathan Wyterlin <jwyterlin@thoughtworks.com>
2020-06-25 14:21:34 -0300

    Use NatFonts in the font of detail label in ExpansionPanelViewController

--- SampleApp/Sources/Sample/Components/ExpansionPanel/ExpansionPanelViewController.swift
+++ SampleApp/Sources/Sample/Components/ExpansionPanel/ExpansionPanelViewController.swift
@@ -16,7 +16,7 @@ class ExpansionPanelViewController: UIViewController, SampleItem {
         label.text = "As for the Hobbits of the Shire, with whom these tales are concerned, in the days of their peace and prosperity they were a merry folk. They dressed in bright colors, being notably fond of yellow and green; but they seldom wore shoes, since their feet had tough leathery soles and were clad in a thick curling hair, much like the hair of their heads, which was commonly brown."
         label.numberOfLines = 0
         label.textColor = NatColors.highEmphasis
-        label.font = Fonts.body1
+        label.font = NatFonts.font(ofSize: .body1, withWeight: .regular)
         return label
     }()
     // swiftlint:enable line_length
