a360002d1d40719a01563cd38cef8ae1dbc79ab3
Vanessa Medeiros <vanessa@limamedeiros.com>
2020-07-16 17:31:14 -0300

    refactor: 💡 Changes label padding constraints in standard badge

--- Sources/Public/Components/Badge/NatBadge.swift
+++ Sources/Public/Components/Badge/NatBadge.swift
@@ -64,11 +64,10 @@ public final class NatBadge: UIView {
         translatesAutoresizingMaskIntoConstraints = false
 
         let constraints = [
-            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -2),
+            label.trailingAnchor.constraint(equalTo: trailingAnchor, constant: -4),
             label.topAnchor.constraint(equalTo: topAnchor),
             label.bottomAnchor.constraint(equalTo: bottomAnchor),
-            label.widthAnchor.constraint(greaterThanOrEqualToConstant: 10),
-            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 2)
+            label.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 4)
         ]
 
         NSLayoutConstraint.activate(constraints)
