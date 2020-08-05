9f44ce652f3fe18d3ea25e1f1f796a00e7b55db8
rov <valadares.thel@gmail.com>
2020-08-05 19:10:08 -0300

    refactor: 💡 Moves AppBarSnapshotTests to snapshot target

--- NatDSSnapShotTests/AppBar/AppBar+Snapshot+Tests.swift
+++ NatDSSnapShotTests/AppBar/AppBar+Snapshot+Tests.swift
@@ -1,55 +1,54 @@
-import FBSnapshotTestCase
+import XCTest
+import SnapshotTesting
 
 @testable import NatDS
 
-class AppBarSnapshotTests: FBSnapshotTestCase {
-    var window: UIWindow!
+class AppBarSnapshotTests: XCTestCase {
     var systemUnderTest: UINavigationController!
 
     override func setUp() {
         super.setUp()
 
         DesignSystem().configure(with: .natura)
-
         systemUnderTest = UINavigationController(rootViewController: UIViewController())
-        window = UIWindow(frame: systemUnderTest.view.frame)
-
-        window.addSubview(systemUnderTest.view)
     }
 
     func test_appbar_style_default_hasValidSnapshot() {
         systemUnderTest.configure(style: .default)
 
-        FBSnapshotVerifyView(systemUnderTest.view)
+        assertSnapshot(matching: systemUnderTest, as: .image)
     }
 
     func test_appbar_style_default_logo_hasValidSnapshot() {
         systemUnderTest.configure(style: .default)
         systemUnderTest.visibleViewController?.configure(titleStyle: .logo)
 
-        FBSnapshotVerifyView(systemUnderTest.view)
+        assertSnapshot(matching: systemUnderTest, as: .image)
     }
 
     func test_appbar_style_default_title_hasValidSnapshot() {
         systemUnderTest.configure(style: .default)
         systemUnderTest.visibleViewController?.configure(titleStyle: .title("New Title"))
 
-        FBSnapshotVerifyView(systemUnderTest.view)
+        assertSnapshot(matching: systemUnderTest, as: .image)
     }
 
     func test_appbar_style_default_rightbuttons_hasValidSnapshot() {
         systemUnderTest.configure(style: .default)
+        let barButtonItem = UIBarButtonItem(
+            icon: .outlinedActionCalendar,
+            action: nil,
+            target: nil
+        )
+        let barButtonItemWithBadge = UIBarButtonItem(
+            icon: .outlinedAlertNotification,
+            action: nil,
+            target: nil
+        )
 
-        let barButtonItem = UIBarButtonItem(icon: .outlinedActionCalendar,
-                                            action: nil,
-                                            target: nil)
-        let barButtonItemWithBadge = UIBarButtonItem(icon: .outlinedAlertNotification,
-                                                     action: nil,
-                                                     target: nil)
         barButtonItemWithBadge.setBadgeValue(99)
-
         systemUnderTest.visibleViewController?.configure(buttons: [barButtonItemWithBadge, barButtonItem])
 
-        FBSnapshotVerifyView(systemUnderTest.view)
+        assertSnapshot(matching: systemUnderTest, as: .image)
     }
 }
