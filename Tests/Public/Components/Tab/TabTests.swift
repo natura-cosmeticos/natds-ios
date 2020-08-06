c7771ea457d8e0c718a982249cbe51d2bc75857e
rov <valadares.thel@gmail.com>
2020-08-06 14:50:46 -0300

    refactor: 💡 Moves Tab snapshot tests to right target

--- Tests/Public/Components/Tab/TabTests.swift
+++ Tests/Public/Components/Tab/TabTests.swift
@@ -1,9 +1,8 @@
-import FBSnapshotTestCase
+import XCTest
 @testable import NatDS
 
-class TabTests: FBSnapshotTestCase {
-
-    var sut: Tab!
+final class TabTests: XCTestCase {
+    var systemUnderTest: Tab!
     var superView: UIView!
 
     var delegateMock: TabDelegateMock!
@@ -16,77 +15,50 @@ class TabTests: FBSnapshotTestCase {
         superView = UIView(frame: CGRect(x: 0, y: 0, width: 328, height: 60))
         superView.backgroundColor = .white
 
-        sut = Tab()
-        sut.delegate = delegateMock
-        sut.frame = CGRect(x: 0, y: 0, width: 328, height: 48)
+        systemUnderTest = Tab()
+        systemUnderTest.delegate = delegateMock
+        systemUnderTest.frame = CGRect(x: 0, y: 0, width: 328, height: 48)
 
-        superView.addSubview(sut)
-    }
-
-    func test_init_whenHas3Tabs_returnsTabWith3TabItemViewsValidSnapshot() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
-        sut.insertTab(title: "Tab 3")
-
-        FBSnapshotVerifyView(superView)
-    }
-
-    func test_whenHas2TabsAndSelectedTabIs2_returnsTabWith2SelectedValidSnapshot() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
-
-        sut.selectedSegmentedIndex = 1
-
-        FBSnapshotVerifyView(superView)
-    }
-
-    func test_whenHas3TabsAndSelectedTabIs2_returnsTabWith2SelectedValidSnapshot() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
-        sut.insertTab(title: "Tab 3")
-
-        sut.selectedSegmentedIndex = 1
-
-        FBSnapshotVerifyView(superView)
+        superView.addSubview(systemUnderTest)
     }
 
     func test_selectedSegmentedIndex_whenIndexNotExistInTabs_returnsSelectedSegmentedIndexAs0() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
-        sut.insertTab(title: "Tab 3")
-        sut.selectedSegmentedIndex = 100
+        systemUnderTest.insertTab(title: "Tab 1")
+        systemUnderTest.insertTab(title: "Tab 2")
+        systemUnderTest.insertTab(title: "Tab 3")
+        systemUnderTest.selectedSegmentedIndex = 100
 
-        XCTAssertEqual(sut.selectedSegmentedIndex, 0)
+        XCTAssertEqual(systemUnderTest.selectedSegmentedIndex, 0)
     }
 
     func test_selectedSegmentedIndex_whenIndexNotExistInTabsAndTabsIsEmpty_returnsSelectedSegmentedIndexAs0() {
-        sut.selectedSegmentedIndex = 100
+        systemUnderTest.selectedSegmentedIndex = 100
 
-        XCTAssertEqual(sut.selectedSegmentedIndex, 0)
+        XCTAssertEqual(systemUnderTest.selectedSegmentedIndex, 0)
     }
 
     func test_selectedSegmentedIndex_whenIndexIsNegativeValueAndTabsIsEmpty_returnsSelectedSegmentedIndexAs0() {
-        sut.selectedSegmentedIndex = -1
+        systemUnderTest.selectedSegmentedIndex = -1
 
-        XCTAssertEqual(sut.selectedSegmentedIndex, 0)
+        XCTAssertEqual(systemUnderTest.selectedSegmentedIndex, 0)
     }
 
     func test_selectedSegmentedIndex_whenIndexIsNegativeValue_returnsSelectedSegmentedIndexAs0() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
-        sut.insertTab(title: "Tab 3")
-        sut.selectedSegmentedIndex = -1
+        systemUnderTest.insertTab(title: "Tab 1")
+        systemUnderTest.insertTab(title: "Tab 2")
+        systemUnderTest.insertTab(title: "Tab 3")
+        systemUnderTest.selectedSegmentedIndex = -1
 
-        XCTAssertEqual(sut.selectedSegmentedIndex, 0)
+        XCTAssertEqual(systemUnderTest.selectedSegmentedIndex, 0)
     }
 
     func test_selectedSegmentedIndex_whenHasDelegate_returnSelectedSegmentedIndexAndCallOnce() {
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
+        systemUnderTest.insertTab(title: "Tab 1")
+        systemUnderTest.insertTab(title: "Tab 2")
 
         let expectIndex = 1
 
-        sut.selectedSegmentedIndex = expectIndex
+        systemUnderTest.selectedSegmentedIndex = expectIndex
 
         XCTAssertEqual(delegateMock.invokedDidChangeSelectedSegmented.count, 1)
         XCTAssertEqual(delegateMock.invokedDidChangeSelectedSegmented.index, expectIndex)
@@ -95,11 +67,11 @@ class TabTests: FBSnapshotTestCase {
     func test_didTapTabItem_whenImplementsTabItemViewDelegate_returnsSelectedSegmentedIndexTapped() {
         let expectIndex = 1
 
-        sut.insertTab(title: "Tab 1")
-        sut.insertTab(title: "Tab 2")
+        systemUnderTest.insertTab(title: "Tab 1")
+        systemUnderTest.insertTab(title: "Tab 2")
 
-        sut.didTapTabItem(sut.tabs[1])
+        systemUnderTest.didTapTabItem(systemUnderTest.tabs[1])
 
-        XCTAssertEqual(sut.selectedSegmentedIndex, expectIndex)
+        XCTAssertEqual(systemUnderTest.selectedSegmentedIndex, expectIndex)
     }
 }
