ea721e4f5106d1fbe3bf1c79a7a0f4ebf3a6133f
rov <valadares.thel@gmail.com>
2020-08-20 14:47:17 -0300

    refactor: 💡 Creates files to separate code and context

--- Sources/Core/DomainFunctions/GetTokensFromTheme.swift
+++ Sources/Core/DomainFunctions/GetTokensFromTheme.swift
@@ -4,27 +4,3 @@ func getTokenFromTheme<T>(_ keyPath: KeyPath<TokensProtocol, T>) -> T {
         fatalError: designSystemFatalError
     ).tokens[keyPath: keyPath]
 }
-
-func getUIColorFromTokens(_ keyPath: KeyPath<TokensProtocol, String>) -> UIColor {
-    getOrCreatedCachedColor(
-        colorHex: getTokenFromTheme(keyPath),
-        storage: ConfigurationStorage.shared,
-        fatalError: { fatalError("Not a token color") }
-    )
-}
-
-func getOrCreatedCachedColor(
-    colorHex: String,
-    storage: ConfigurationStorage,
-    fatalError: () -> Never) -> UIColor {
-
-    if let color = storage.cachedColors[colorHex] {
-        return color
-    }
-
-    guard let color = UIColor(hex: colorHex) else { fatalError() }
-
-    storage.cachedColors[colorHex] = color
-
-    return color
-}
