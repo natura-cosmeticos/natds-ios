136229e494c6b78ba2aaebc8d995d14fc765e9d0
Rubens Gondek <rubens.gondek@thoughtworks.com>
2020-09-18 11:00:16 -0300

    test: 💍 Add unit tests for NavigationDrawerCell

--- Tests/Public/Components/NavigationDrawer/NavigationDrawerItemCellTests.swift
+++ Tests/Public/Components/NavigationDrawer/NavigationDrawerItemCellTests.swift
@@ -22,4 +22,32 @@ final class NavigationDrawerItemCellTests: XCTestCase {
     func test_init_hasSelectionStyleAsNone() {
         XCTAssertEqual(systemUnderTest.selectionStyle, .none)
     }
+
+    func test_init_tagTextAsNil() {
+        XCTAssertEqual(systemUnderTest.tagText, nil)
+    }
+
+    func test_init_tagView_isHidden() {
+        let tagView = systemUnderTest.contentView.subviews[3]
+        XCTAssert(tagView is NatTag)
+        XCTAssertEqual(tagView.isHidden, true)
+    }
+
+    func test_setTagText_showsTagView() throws {
+        systemUnderTest.tagText = "Tag"
+
+        let tagView = try XCTUnwrap(systemUnderTest.contentView.subviews[3] as? NatTag)
+        XCTAssertEqual(tagView.isHidden, false)
+        let label = try XCTUnwrap(tagView.subviews.first as? UILabel)
+        XCTAssertEqual(label.text, "Tag")
+    }
+
+    func test_setTagText_asNil_hidesTagView() {
+        systemUnderTest.tagText = "Tag"
+        let tagView = systemUnderTest.contentView.subviews[3]
+        XCTAssertEqual(tagView.isHidden, false)
+
+        systemUnderTest.tagText = nil
+        XCTAssertEqual(tagView.isHidden, true)
+    }
 }
