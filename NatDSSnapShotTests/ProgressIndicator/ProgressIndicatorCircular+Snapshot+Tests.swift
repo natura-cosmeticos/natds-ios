c2e0b9622193665e03bc63c09e4585e466cc8d9b
Vitor Pires <vitor.pires@thoughtworks.com>
2020-11-19 19:13:23 -0300

    test: 💍 Adds snapshot tests

---
+++ NatDSSnapShotTests/ProgressIndicator/ProgressIndicatorCircular+Snapshot+Tests.swift
@@ -0,0 +1,39 @@
+import XCTest
+import SnapshotTesting
+
+@testable import NatDS
+
+final class ProgressIndicatorCircularSnapshotTests: XCTestCase {
+    var superview: UIView!
+
+    override func setUp() {
+        super.setUp()
+
+        superview = UIView(frame: CGRect(x: 0, y: 0, width: 200, height: 200))
+        superview.backgroundColor = .white
+
+        ConfigurationStorage.shared.currentTheme = NaturaLightTheme()
+    }
+
+    func test_progress_indicator_circular_hasValidSnapshot() {
+        let systemUnderTest = ProgressIndicatorCircular()
+
+        systemUnderTest.configure()
+        superview.addSubview(systemUnderTest)
+        addConstraints(systemUnderTest)
+
+        assertSnapshot(matching: superview, as: .image(precision: 0.97))
+    }
+
+    private func addConstraints(_ systemUnderTest: UIView) {
+
+        systemUnderTest.translatesAutoresizingMaskIntoConstraints = false
+
+        let constraints = [
+            systemUnderTest.topAnchor.constraint(equalTo: superview.topAnchor, constant: 40),
+            systemUnderTest.centerXAnchor.constraint(equalTo: superview.centerXAnchor),
+            systemUnderTest.centerYAnchor.constraint(equalTo: superview.centerYAnchor)
+        ]
+        NSLayoutConstraint.activate(constraints)
+    }
+}
