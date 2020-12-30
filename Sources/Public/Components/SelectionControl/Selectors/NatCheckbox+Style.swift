a2e94df14a51abdf6cce0481dd2316c40893ad26
Victor Melo <victor.contas@outlook.com>
2020-12-30 11:36:48 -0300

    feat: NatCheckbox style code organization

--- Sources/Public/Components/SelectionControl/Selectors/NatCheckbox+Style.swift
+++ Sources/Public/Components/SelectionControl/Selectors/NatCheckbox+Style.swift
@@ -1,5 +1,8 @@
 extension NatCheckbox {
     struct Style {
+        
+        // MARK: Static Properties
+        
         static var `default`: Style {
             Style(uncheckedBorderColor: NatColors.mediumEmphasis,
                   uncheckedBackgroundColor: .clear,
@@ -7,12 +10,16 @@ extension NatCheckbox {
                   checkedBackgroundColor: NatColors.primary,
                   checkmarkColor: .white)
         }
+        
+        // MARK: Properties
 
         let uncheckedBorderColor: UIColor
         let uncheckedBackgroundColor: UIColor
         let checkedBorderColor: UIColor
         let checkedBackgroundColor: UIColor
         let checkmarkColor: UIColor
+        
+        // MARK: Public Methods
 
         func borderColor(_ isSelected: Bool, isEnabled: Bool) -> UIColor {
             isEnabled ? enabledBorderColor(isSelected) : NatColors.lowEmphasis
@@ -21,6 +28,8 @@ extension NatCheckbox {
         func backgroundColor(_ isSelected: Bool, isEnabled: Bool) -> UIColor {
             isEnabled ? enabledBackgroundColor(isSelected) : disabledBackgroundColor(isSelected)
         }
+        
+        // MARK: Private Methods
 
         private func enabledBorderColor(_ isSelected: Bool) -> UIColor {
             isSelected ? checkedBorderColor: uncheckedBorderColor
