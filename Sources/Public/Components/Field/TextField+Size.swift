4adb0b06083f3df2c2728351f7c0c4a4857e8022
Penélope Araújo <pmpa@cin.ufpe.br>
2021-03-09 17:57:31 -0300

    docs: add docs to textField methods and properties

--- Sources/Public/Components/Field/TextField+Size.swift
+++ Sources/Public/Components/Field/TextField+Size.swift
@@ -1,4 +1,12 @@
 extension TextField {
+    /**
+     Size is an enum that represents the possible sizes for the TextField height.
+     The default size is `mediumX`.
+     
+     These are all sizes allowed for a TextField:
+     - medium
+     - mediumX
+     */
     public enum Size: CaseIterable {
         case medium
         case mediumX
--- Sources/Public/Components/Field/TextField.swift
+++ Sources/Public/Components/Field/TextField.swift
@@ -1,24 +1,54 @@
 /**
  TextField is a class that represents a component from the design system.
+ 
+ It can be configured with 2 different sizes:
+ - MediumX (default)
+ - Medium
+ 
+ Example of usage:
+ 
+            let textField = TextField()
+            textfield.configure(size: .medium)
+ 
  The textField styles, keyboards, capitalization and autocorrection properties changes according to the chosen type.
 
  This TextField has 4 types:
- - Text
+ - Text (default type)
  - Name
  - Number
  - Password
 
  Example of usage:
  
-        textField.type = .text
-        textField.type = .name
-        textField.type = .number
-        textField.type = .password(keyboardType: .numberPad
+        textField.configure(type: .text)
+        textField.configure(type: .password(keyboardType: .numberPad)
  
- This TextField has 3 States:
- - enable
- - active
- - error
+ The TextField has 3 states for feedback, that can be configured with a message:
+ - Error
+ - Success
+ - None (clear error/success states)
+ 
+ Example of usage:
+ 
+        textField.configure(state: .error, with: "Error")
+ 
+ It also can be configured as a required field, 'read-only' field or disabled textField. Each interaction state has its pre-defined style:
+ 
+        textField.configure(isEnabled: false)
+        textField.configure(readOnly: true)
+        textField.configure(required: true)
+ 
+ The TextField also has an action item on the right, which can be configured with an icon from NatDSIcons, a local image or a remote image:
+ 
+        textField.configure(icon: getIcon(.outlinedDefaultMockup)) {
+            // action
+        }
+        field.configure(image: UIImage(named: "imageName")) {
+            // action
+        }
+        field.configure(remoteImageURL: URL(string: "urlForImage")) {
+            // action
+        }
 
  There are properties that changes the textfield styles as well.
 
@@ -45,13 +75,13 @@ public class TextField: UIView {
     
     // MARK: - Public vars
     
-    /// Label text that is always displayed above textfield
+    /// A string with the text to be always displayed above textfield
     public var title: String? {
         get { titleLabel.text }
         set { titleLabel.text = newValue }
     }
     
-    /// The text from the textfield
+    /// A string with the text inside the textField
     public var text: String? {
         get { textField.text }
         set {
@@ -60,20 +90,20 @@ public class TextField: UIView {
         }
     }
     
-    /// Hint text to display when the textfield is empty
+    /// A string with the hint text to be displayed when the textField is empty
     public var placeholder: String? {
         get { textField.placeholder }
         set { textField.placeholder = newValue}
     }
     
-    /// Hint text that is always displayed below textfield
+    /// A string with a helper text to be displayed below textField
     public var helper: String? {
         didSet {
             helperLabel.text = helper
         }
     }
     
-    /// Text that alerts about an error
+    /// A string with a text that alerts about an error. It triggers the state `.error` for the textField.
     public var error: String? {
         didSet {
             configure(state: .error, with: error)
@@ -87,24 +117,29 @@ public class TextField: UIView {
         }
     }
     
+    /// A boolean that indicates if filling the textField is mandatory
     public var required: Bool = false {
         didSet {
             handleRequired()
         }
     }
     
+    /// A boolean that indicates if the textField is filled with info that can't be changed
     public var readOnly: Bool = false {
         didSet {
             self.interactionState = readOnly ? .readOnly : .enabled
         }
     }
     
+    /// A boolean that indicates if the textField is enabled or disabled
     public var isEnabled: Bool = true {
         didSet {
             self.interactionState = isEnabled ? .enabled : .disabled
         }
     }
     
+    
+    /// The size of the textfield, chosen from the `TextField.Size` enum
     public var size: Size = .mediumX {
         didSet {
             updateTextFieldHeightConstraint()
@@ -454,7 +489,7 @@ extension TextField {
         actionImageView.bottomAnchor.constraint(equalTo: textField.bottomAnchor).isActive = true
         actionImageView.widthAnchor.constraint(equalToConstant: getTokenFromTheme(\.sizeLarge)).isActive = true
     }
-    
+
     public func showVisibilityIcon() {
         if self.type.secureTextEntry {
             if self.subviews.contains(iconButtonGeneral) {
@@ -522,43 +557,67 @@ extension TextField: UITextFieldDelegate {
 extension TextField {
     // MARK: - Public Methods
     
+    /// Sets the textField title, displayed above the textField box
+    /// - Parameter title: A string with the text for the title
     public func configure(title: String) {
         self.title = title
     }
     
+    /// Sets the textField placeholder, a hint text displayed inside the textField box when it is empy
+    /// - Parameter placeholder: A string with the text for the placeholder
     public func configure(placeholder: String) {
         self.placeholder = placeholder
     }
     
+    /// Sets the textField size, which changes it's height
+    /// - Parameter size: An option from `TextField.Size` enum
     public func configure(size: Size) {
         self.size = size
     }
     
+    /// Sets the textField type, which changes it's configuration for keyboard, secure text, capitalization and autocorrection
+    /// - Parameter type: An option from `TextFieldType`
     public func configure(type: TextFieldType) {
         self.type = type
     }
     
+    /// Sets the state of the textField and a text to describe the state
+    /// - Parameters:
+    ///   - state: An option from `TextField.FeedbackState`: error, success or none
+    ///   - text: A string with the text to be displayed below the textField box. If the state is `error` or `success`, the text will have a small icon next to it. If the state is  `none`, the text will be displayed as a helper text.
     public func configure(state: FeedbackState, with text: String?) {
         self.state = state
         self.helper = text
     }
     
+    /// Sets if the textField is required, when its filling is mandatory
+    /// - Parameter required: A boolean indicating if the textField is required
     public func configure(required: Bool) {
         self.required = required
     }
     
+    /// Sets if the textField is enabled or disabled
+    /// - Parameter isEnabled: A boolean indicating if the textField is enabled
     public func configure(isEnabled: Bool) {
         self.isEnabled = isEnabled
     }
     
+    /// Sets if the textField is read only
+    /// - Parameter readOnly: A boolean indicating if the textField is read only
     public func configure(readOnly: Bool) {
         self.readOnly = readOnly
     }
     
+    /// Sets a helper text to be displayed below the textField box
+    /// - Parameter helperText: A string with the helper text
     public func configure(helperText: String) {
         self.helper = helperText
     }
     
+    /// Sets an icon to be displayed inside the textField box, aligned with it's right side, and the action to be executed when the icon is tapped.
+    /// - Parameters:
+    ///   - icon: An icon from NatDSIcons, which is sent as a string. Example: `getIcon(.outlinedDefaultMockup)`
+    ///   - action: A block of code to be executed when the icon receives a tap.
     public func configure(icon: String?, with action: @escaping () -> Void) {
         configureRemoveAction()
         hideVisibilityIcon()
@@ -573,6 +632,10 @@ extension TextField {
         }
     }
     
+    /// Sets a local image to be displayed inside the textField box, aligned with the right side, and the action to be executed when it is tapped.
+    /// - Parameters:
+    ///   - image: An UIImage to be displayed in the textField right edge
+    ///   - action: A block of code to be executed when the image is tapped
     public func configure(image: UIImage?, with action: @escaping () -> Void) {
         configureRemoveAction()
         hideVisibilityIcon()
@@ -584,6 +647,10 @@ extension TextField {
         iconButtonGeneral.alpha = 0
     }
     
+    /// Sets a remote image to be displayed inside the textField box, aligned with the right edge, and the action to be executed when it is tapped.
+    /// - Parameters:
+    ///   - remoteImageURL: A URL that contains the remote address to the image. Example: `URL(string: "http://myimage")!`
+    ///   - action: A block of code the be executed when the image is tapped
     public func configure(remoteImageURL: URL, with action: @escaping () -> Void) {
         configureRemoveAction()
         hideVisibilityIcon()
@@ -595,6 +662,7 @@ extension TextField {
         iconButtonGeneral.alpha = 0
     }
     
+    /// Removes the action item, whether it's an icon or an image
     public func configureRemoveAction() {
         if self.subviews.contains(iconButtonGeneral) {
             iconButtonGeneral.removeFromSuperview()
@@ -604,10 +672,12 @@ extension TextField {
         }
     }
     
+    /// Sets the eye icon to show and hide the textField content. It can only be used if the textField has the `password` type.
     public func configureShowVisibilityIcon() {
         showVisibilityIcon()
     }
     
+    /// Removes the eye icon from `password` type textFields.
     public func configureRemoveVisibilityIcon() {
         hideVisibilityIcon()
     }
