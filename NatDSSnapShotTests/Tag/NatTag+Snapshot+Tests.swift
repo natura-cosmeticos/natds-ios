e3f1b37ed214fb96b65f43faa3f966dcc2e4eb60
Vitor Pires <vitor.pires@thoughtworks.com>
2021-03-22 11:01:04 -0300

    test: 💍 Adds snapshot tests

--- NatDSSnapShotTests/Tag/NatTag+Snapshot+Tests.swift
+++ NatDSSnapShotTests/Tag/NatTag+Snapshot+Tests.swift
@@ -15,6 +15,7 @@ final class NatTagSnapshotTests: XCTestCase {
     func test_tag_style_default_alert_hasValidSnapshot() {
         let sut = NatTag(style: .defaultAlert)
         sut.configure(text: "New")
+        sut.configure(color: .alert)
 
         assertSnapshot(matching: sut, as: .image)
     }
@@ -22,14 +23,34 @@ final class NatTagSnapshotTests: XCTestCase {
     func test_tag_style_left_alert_hasValidSnapshot() {
         let sut = NatTag(style: .leftAlert)
         sut.configure(text: "New")
-        
+        sut.configure(color: .alert)
+
         assertSnapshot(matching: sut, as: .image)
     }
     
     func test_tag_style_right_alert_hasValidSnapshot() {
         let sut = NatTag(style: .rightAlert)
         sut.configure(text: "New")
+        sut.configure(color: .alert)
         
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_tag_size_standard_hasValidSnapshot() {
+        let sut = NatTag(style: .rightAlert)
+        sut.configure(text: "New")
+        sut.configure(color: .alert)
+        sut.configure(size: .standard)
+
+        assertSnapshot(matching: sut, as: .image)
+    }
+
+    func test_tag_size_small_hasValidSnapshot() {
+        let sut = NatTag(style: .rightAlert)
+        sut.configure(text: "New")
+        sut.configure(color: .alert)
+        sut.configure(size: .small)
+
         assertSnapshot(matching: sut, as: .image)
     }
 }
