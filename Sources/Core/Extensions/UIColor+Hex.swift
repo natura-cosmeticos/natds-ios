6c6040d2d70156caaf775a77b5f16022ce7c64b3
Penélope Araújo <pmpa@cin.ufpe.br>
2021-09-22 11:33:22 -0300

    feat: add method to get colors from attributes and refactor color to accept 8-digit hex

--- Sources/Core/Extensions/UIColor+Hex.swift
+++ Sources/Core/Extensions/UIColor+Hex.swift
@@ -1,21 +1,42 @@
+// swiftlint:disable identifier_name
 extension UIColor {
-    convenience init?(hex: String, alpha: CGFloat = 1.0) {
-        var cString: String = hex.trimmingCharacters(in: .whitespacesAndNewlines).uppercased()
-        if cString.hasPrefix("#") {
-            cString.remove(at: cString.startIndex)
+    convenience init?(hex: String) {
+
+        var hexString: String = hex.trimmingCharacters(in: CharacterSet.whitespacesAndNewlines)
+
+        if hexString.hasPrefix("#") {
+            hexString.removeFirst()
         }
-        if (cString.count) != 6 {
+
+        if hexString.count == 3 {
+            var fullString = ""
+            for char in hexString {
+                fullString.append(char)
+                fullString.append(char)
+            }
+            hexString = fullString
+        }
+
+        if hexString.count == 6 {
+            hexString.append("FF")
+        }
+
+        if hexString.count != 8 {
             return nil
         }
-        var rgbValue: UInt32 = 0
-        Scanner(string: cString).scanHexInt32(&rgbValue)
-        self.init(red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
-                  green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
-                  blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
-                  alpha: alpha)
-    }
 
-    static func hexColorOrDefault(_ hex: String) -> UIColor {
-        return UIColor(hex: hex) ?? .black
+        let scanner = Scanner(string: hexString)
+        var color: UInt64 = 0
+        scanner.scanHexInt64(&color)
+        let mask = 0x00000000FF
+        let r = Int(color >> 24) & mask
+        let g = Int(color >> 16) & mask
+        let b = Int(color >> 8) & mask
+        let a = Int(color) & mask
+        let red   = CGFloat(r) / 255.0
+        let green = CGFloat(g) / 255.0
+        let blue  = CGFloat(b) / 255.0
+        let alpha = CGFloat(a) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
 }
