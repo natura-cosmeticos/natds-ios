2b7d18f1927f7d134c799fbdcc14202b1d662b3e
Penélope Araújo <pmpa@cin.ufpe.br>
2021-10-06 11:52:42 -0300

    chore: get most recent tag from theme and update it from makefile

--- scripts/update_themes.sh
+++ scripts/update_themes.sh
@@ -1,18 +1,21 @@
-#!/bin/zsh
+#!/bin/bash
 
-domain='https://github.com/natura-cosmeticos/natds-commons/archive/refs/tags/@naturacosmeticos/natds-themes@'
+# get most recent themes tag and save it to version-theme
+git ls-remote --tags --sort="v:refname" https://github.com/natura-cosmeticos/natds-commons.git | grep "@naturacosmeticos/natds-themes@[0-9]*\.[0-9]*\.[0-9]*" | tail -n1 | sed 's/.*\///; s/\^{}//' > version-theme.txt
+
+# get commons from github
+domain='https://github.com/natura-cosmeticos/natds-commons/archive/refs/tags/@naturacosmeticos/'
+version=`cat version-theme.txt`
 extension='.tar.gz'
 
-echo "Downloading from $domain${1}$extension"
+echo "Downloading from $domain$version$extension"
 mkdir /tmp/natds
-curl $domain${1}$extension -J -L -o /tmp/natds/Themes.tar.gz
-
-echo "Saving at /tmp"
+curl $domain$version$extension -J -L -o /tmp/natds/Themes.tar.gz
 tar -xzvf /tmp/natds/Themes.tar.gz -C /tmp/natds
 
-cd /tmp/natds/*/packages/natds-themes/build/ios/theme
-echo ${1} > themes-version.txt
+# copy themes folder from commons to project source files
+cp -r /tmp/natds/*/packages/natds-themes/build/ios/theme ./Sources/Core/Theme/SingleSourceOfTruth
 
-cp -r /tmp/natds/*/packages/natds-themes/build/ios/theme ~/Desktop
-
-echo -e "\e[1;33m'Themes' folder is now in your Desktop, move it to your project \e[0m"
+echo "---------------------------------------------------------------"
+echo "Themes copied to project source files"
+echo "---------------------------------------------------------------"
