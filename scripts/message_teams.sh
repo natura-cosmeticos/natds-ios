c2b4bddf18279302b2c95e63695e4c11b1b721df
Penélope Araújo <pmpa@cin.ufpe.br>
2021-11-30 14:48:08 -0300

    feat: automate release notification at teams

--- scripts/message_teams.sh
+++ scripts/message_teams.sh
@@ -1,42 +1,35 @@
 #!/bin/bash
 
+# create new file to edit
+cp ./scripts/message_teams.json ./scripts/updated_message_teams.json
+
 # get and update version at message
 NEW_VERSION=`cat ./version.txt`
-sed -i '' 's/VERSIONVALUE/'$NEW_VERSION'/g' ./scripts/message_teams.json
+sed -i '' 's/VERSIONVALUE/'$NEW_VERSION'/g' ./scripts/updated_message_teams.json
 
-releaseline='https://github.com/natura-cosmeticos/natds-ios/compare/'
+releaselinecontent='https://github.com/natura-cosmeticos/natds-ios/compare/'
 major=`echo $NEW_VERSION | cut -d. -f1`
 minor=`echo $NEW_VERSION | cut -d. -f2`
 patch=`echo $NEW_VERSION | cut -d. -f3`
 rgxversion="$major\.$minor\.$patch" # regex for new version
 
-# clear files
-if [ -f ./scripts/message_release.txt ]; then
-    : > ./scripts/message_release.txt
-fi
-
-if [ -f ./scripts/message_text.txt ]; then
-    : > ./scripts/message_text.txt
-fi
-
 # save last version output to file
-awk 's{print $0 > "./scripts/message_release.txt"}; $0~v {print $0 > "./scripts/message_release.txt"; s=1; next}; (!($0~v) && $0~l) {s=0};' RS= v=$rgxversion l=$releaseline CHANGELOG.md
+awk 's{print $0 > "./scripts/message_release.txt"}; $0~v {print $0 > "./scripts/message_release.txt"; s=1; next}; (!($0~v) && $0~l) {s=0};' RS= v=$rgxversion l=$releaselinecontent CHANGELOG.md
 # erase last line of file
 sed -i '' -e '$ d' ./scripts/message_release.txt
 
-# write message release to message text removing breaklines and adding \n 
+# write release message with line breaks
 awk '{printf "%s\\\n ",$0 > "./scripts/message_text.txt"} END {print ""}' ./scripts/message_release.txt
 
+# update release message at json
 CHANGELOG_UPDATES=`cat ./scripts/message_text.txt`
-sed -i '' "s|CHANGELOGMESSAGE|$CHANGELOG_UPDATES|g" ./scripts/message_teams.json
+sed -i '' "s|CHANGELOGMESSAGE|$CHANGELOG_UPDATES|g" ./scripts/updated_message_teams.json
 
 # send message
-message=`cat ./scripts/message_teams.json`
+message=`cat ./scripts/updated_message_teams.json`
 curl -H 'Content-Type: application/json' -d "$message" "$TEAMS_RELEASE_WEBHOOK"
 
-# reset version and changelog updates at message
-# sed -i '' "s|$MESSAGE_VERSION|VERSIONVALUE|g" ./scripts/message_teams.json
-# sed -i '' "s|$CHANGELOG_UPDATES|CHANGELOGMESSAGE|g" ./scripts/message_teams.json
-
-# sed -i '' 's/'$MESSAGE_VERSION'/VERSIONVALUE/g' ./scripts/message_teams.json
-# sed -i '' 's/'$CHANGELOG_UPDATES'/CHANGELOGMESSAGE/g' ./scripts/message_teams.json
+# remove helper files
+rm ./scripts/updated_message_teams.json
+rm ./scripts/message_release.txt
+rm ./scripts/message_text.txt
