089756ea5ed2b2709ef20defae207ef960095dbc
Penélope Araújo <pmpa@cin.ufpe.br>
2022-04-13 16:17:04 -0300

    refactor: remove illustration sad/lined image
    remove IllustrationIcon component, as its not a component or icon defined by the Design System
    documentation
    
    BREAKING CHANGE: remove IllustrationIcon component

--- Tests/Helpers/AssetsHelperTests.swift
+++ Tests/Helpers/AssetsHelperTests.swift
@@ -2,11 +2,7 @@ import XCTest
 @testable import NatDS
 
 class AssetsHelperTests: XCTestCase {
-    
-    func test_imageFromName_returnsNonNullImage() {
-        let image = AssetsHelper.image(from: "Illustration/Lined/Sad")
-        XCTAssertNotNil(image)
-    }
+
     func test_imageFromInvalidName_returnsNullImage() {
         let image = AssetsHelper.image(from: "")
         XCTAssertNil(image)
