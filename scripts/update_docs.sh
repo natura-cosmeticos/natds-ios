f3661efaa4d9e55b9b671be5c55a8570529186f1
Penélope Araújo <pmpa@cin.ufpe.br>
2022-04-28 18:07:21 -0300

    chore: update docs script

--- scripts/update_docs.sh
+++ scripts/update_docs.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# upates docs internal links
+# updates docs internal links
 sed -i'' -e "s|<a href=\"docs/How%20to%20contribute.md\">|<a href="how-to-contribute.html" target=\"_blank\">|g" ./docs/documentation/index.html
 
 sed -i'' -e "s|<a href=\"docs/Getting%20started.md\">|<a href="getting-started.html" target=\"_blank\">|g" ./docs/documentation/index.html
