83e572b8c62684489fda0dedb4ab968fe684e2df
hayna-foursys <131673198+hayna-foursys@users.noreply.github.com>
2023-05-08 09:59:28 -0300

    fix name

--- SampleApp/Sources/Sample/Components/ComponentsSection.swift
+++ SampleApp/Sources/Sample/Components/ComponentsSection.swift
@@ -12,7 +12,7 @@ class ComponentsSection: SampleSection {
         CounterViewController.self,
         CheckboxViewController.self,
         ChipViewController.self,
-        FilterViewController.self,
+        ChipFilterViewController.self,
         DialogItemViewController.self,
         DividerViewController.self,
         ExpansionPanelViewController.self,
