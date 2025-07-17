9e1a30c410556f716589c1b5433f1d3b4c693970
hayna-foursys <hayna.cardoso@foursys.com.br>
2025-07-17 16:14:55 -0300

    Update AssetsHelper.swift

--- Sources/Core/Helpers/AssetsHelper.swift
+++ Sources/Core/Helpers/AssetsHelper.swift
@@ -5,7 +5,12 @@ class AssetsHelper {
     }
     
     static func image(from name: String) -> UIImage? {
-        let bundle = Bundle(for: self)
+        #if SWIFT_PACKAGE
+          let bundle = Bundle.module
+        #else
+          let bundle = Bundle(for: self)
+        #endif
+      
         return UIImage(named: name, in: bundle, compatibleWith: nil)
     }
     
@@ -23,7 +28,12 @@ class AssetsHelper {
     
     private static func registerFont(_ name: String, _ type: FontType) {
         var error: Unmanaged<CFError>?
-        var fontBundle: Bundle? = Bundle(for: self)
+      
+        #if SWIFT_PACKAGE
+          var fontBundle = Bundle.module
+        #else
+          var fontBundle: Bundle? = Bundle(for: self)
+        #endif
         
         guard let bundle = fontBundle else { return }
         guard let pathForResource = bundle.url(forResource: name, withExtension: "ttf") else { return }
